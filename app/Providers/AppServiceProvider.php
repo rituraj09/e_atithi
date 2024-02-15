68524eb9d963847a02fa1261c8696ff38000de2e
riturajai <riturajborgohain@gmail.com>
2024-02-15 12:31:34 +0530

    done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Builder::defaultStringLength(191);
     }
 
     /**
